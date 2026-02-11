950fc2f4aeba986bfc6b6cb2811d7d8a6da7c2b4
Miguel-Angel-Salazar <miguelangelguarnizosalazar@gmail.com>
2026-02-11 16:21:33 +0000

    clase_2

--- analisis asintotico/ejercicio1.py
+++ analisis asintotico/ejercicio1.py
@@ -9,3 +9,27 @@ def funcion(n):
 # si el ciclo no depende de la entrada por lo tanto es constante
 # cuanto llegamos al limite seria el caso Big O, el peor caso de todos por ser probado 
 # para saber cual es el caso Big O lo unico que hago es eliminar nuestras constante y evaluar en esos terminos
+# si se llega el caso en el que una de las funciones pertenece a la otra se pueden juntar las dos f(n) = n**2 y g(n) = n; la funcion completa es n**2 + n, como pertenecen se agrupan
+
+"""
+ejemplo
+
+f(n) =  n**2 + n
+
+cual es la complejidad = n**2 (se pudo agrupar n en n**2)
+cual es el tamaño de la entrada =
+cuantas operaciones se hacen =
+
+
+ejemplo 2
+
+operacion(n)
+i = 1
+while i < n
+    if XXXXXX
+    i = i * 2
+
+su complejidad es logaritmica ya que como se encuentra el while y el resultado final se puede obsevsar que i * 2, y analizando la condicion del while;
+podemos observar que llegara mas rapido que si fuera un ciclo for, por lo tanto es logaritmico
+"""
+
---
+++ analisis asintotico/ejercicio_practico.py
@@ -0,0 +1,44 @@
+"""
+def clave_candado(combinacion=int):
+
+    contraseña = input("Ingrese su contraseña: ")
+
+    for i in range(1):
+        if combinacion == contraseña:
+            print("el candado abre")
+        else:
+            print("contraseña incorrecta")
+
+resultado = clave_candado()
+print(resultado)
+"""
+# _____________________________________________________________________
+"""
+contraseña = input("Ingrese su contraseña: ")
+
+intentos = 0
+
+for i in range(0,1000):
+    clave_generada = str(i).zfill(3) 
+    intentos += 1
+
+    if clave_generada == contraseña:
+        print("el candado abre")
+        print(f"la clave es {clave_generada} y se intentó {intentos} veces")
+        break
+    
+"""
+
+contraseña = input("Ingrese su contraseña")
+n = len(contraseña)
+intentos = 0
+
+for i in range(0, 10**n):
+    clave_generada = str(i).zfill(n) 
+    intentos += 1
+
+    if clave_generada == contraseña:
+        print("el candado abre")
+        print(f"la clave es {clave_generada} y se intentó {intentos} veces")
+        break
+
