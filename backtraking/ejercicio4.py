01ae3bb90c07df137e5d5c69b9841d55e7864fa6
Miguel-Angel-Salazar <miguelangelguarnizosalazar@gmail.com>
2026-03-19 17:13:41 -0500

    codigo corchetes y parentesis 2.0

--- backtraking/ejercicio4.py
+++ backtraking/ejercicio4.py
@@ -21,3 +21,61 @@ def generar_parentesis(n):
 
 if __name__ == "__main__":
     print(generar_parentesis(3))
+
+
+
+#______________________________________________________
+
+def generar_parentesis_corchetes(n):
+    resultado = []
+
+    def backtrack(cadena, abiertos_p, cerrados_p, abiertos_c, cerrados_c, pila):
+        if len(cadena) == 4 * n:
+            resultado.append(cadena)
+            return
+
+        if abiertos_p < n:
+            backtrack(
+                cadena + "(",
+                abiertos_p + 1,
+                cerrados_p,
+                abiertos_c,
+                cerrados_c,
+                pila + "("
+            )
+
+        if abiertos_c < n:
+            backtrack(
+                cadena + "[",
+                abiertos_p,
+                cerrados_p,
+                abiertos_c + 1,
+                cerrados_c,
+                pila + "["
+            )
+
+        if cerrados_p < abiertos_p and pila and pila[-1] == "(":
+            backtrack(
+                cadena + ")",
+                abiertos_p,
+                cerrados_p + 1,
+                abiertos_c,
+                cerrados_c,
+                pila[:-1]
+            )
+
+        if cerrados_c < abiertos_c and pila and pila[-1] == "[":
+            backtrack(
+                cadena + "]",
+                abiertos_p,
+                cerrados_p,
+                abiertos_c,
+                cerrados_c + 1,
+                pila[:-1]
+            )
+
+    backtrack("", 0, 0, 0, 0, "")
+    return resultado
+
+if __name__ == "__main__":
+    print(generar_parentesis_corchetes(2))
