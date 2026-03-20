244c7a3ee132abb83c36d912e2209bb38f6a6523
Miguel-Angel-Salazar <miguelangelguarnizosalazar@gmail.com>
2026-03-20 15:15:36 +0000

    co

--- backtraking/ejercicio4.py
+++ backtraking/ejercicio4.py
@@ -35,44 +35,16 @@ def generar_parentesis_corchetes(n):
             return
 
         if abiertos_p < n:
-            backtrack(
-                cadena + "(",
-                abiertos_p + 1,
-                cerrados_p,
-                abiertos_c,
-                cerrados_c,
-                pila + "("
-            )
+            backtrack(cadena + "(",abiertos_p + 1,cerrados_p,abiertos_c,cerrados_c,pila + "(")
 
         if abiertos_c < n:
-            backtrack(
-                cadena + "[",
-                abiertos_p,
-                cerrados_p,
-                abiertos_c + 1,
-                cerrados_c,
-                pila + "["
-            )
+            backtrack(cadena + "[",abiertos_p,cerrados_p,abiertos_c + 1,cerrados_c,pila + "[")
 
         if cerrados_p < abiertos_p and pila and pila[-1] == "(":
-            backtrack(
-                cadena + ")",
-                abiertos_p,
-                cerrados_p + 1,
-                abiertos_c,
-                cerrados_c,
-                pila[:-1]
-            )
+            backtrack(cadena + ")",abiertos_p,cerrados_p + 1,abiertos_c,cerrados_c,pila[:-1])
 
         if cerrados_c < abiertos_c and pila and pila[-1] == "[":
-            backtrack(
-                cadena + "]",
-                abiertos_p,
-                cerrados_p,
-                abiertos_c,
-                cerrados_c + 1,
-                pila[:-1]
-            )
+            backtrack(cadena + "]",abiertos_p,cerrados_p,abiertos_c,cerrados_c + 1,pila[:-1])
 
     backtrack("", 0, 0, 0, 0, "")
     return resultado
